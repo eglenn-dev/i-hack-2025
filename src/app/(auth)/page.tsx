f4840c425da870e2bbf7d805388fc48000eb00f4
Ethan Glenn <ethan+git@eglenn.dev>
2025-11-01 13:27:33 -0600

    Removed health route, and removed session check from home page

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,17 +1,5 @@
-import { redirect } from "next/navigation";
-import { getSession } from "@/lib/auth/session";
 import LandingPage from "./landing-page";
 
-
 export default async function Home() {
-  const session = await getSession();
-
-  if (session) {
-    redirect("/dashboard");
-  } 
-  return(
-    <>
-      <LandingPage />
-    </>
-  )
+    return <LandingPage />;
 }
